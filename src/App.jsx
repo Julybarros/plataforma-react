41ef86dc9b219b6fb325d46f2aa3b9d3f9278e48
julybarros <julianajulyy22@gmail.com>
2023-03-21 15:20:01 -0300

    aula 21/03

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Menu } from './Components/Menu/Menu';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Blog } from './pages/Blog/Blog';
 import { Contato } from './pages/Contato/Contato';
+import { Usuarios } from './pages/Usuarios/Usuarios';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contato" element={<Contato />} />
+          <Route path="/usuarios" element={<Usuarios/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ export function Home() {
     return (
         <div className="home">
             <h1>Home</h1>
+        
         </div>
     );
 }
